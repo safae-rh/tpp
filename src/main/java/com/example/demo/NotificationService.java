9e8a681f3b082ed5be4cc2a2259f36f582ea8b10
safae-rh <safaarhazouli10@gmail.com>
2025-03-11 15:01:14 +0100

    Delete src directory

--- src/main/java/com/example/demo/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-public class NotificationService {
-	public void envoyerNotification(String message) {
-		// Simule l'envoi d'une notification (par email, SMS, etc.)
-		System.out.println("Notification envoyée : " + message);
-		}
-		}
-
